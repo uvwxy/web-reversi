9f062fb9258d74a9936b2befb1be5e4b9742237c
Paul Smith <code@uvwxy.de>
2016-01-27 20:55:19 +0100

    Add code generated via 'yo angular'

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webReversiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the webReversiApp
+ */
+angular.module('webReversiApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webReversiApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the webReversiApp
+ */
+angular.module('webReversiApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
