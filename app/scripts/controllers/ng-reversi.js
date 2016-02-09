2d8a1d3de44bbb84ffc35b47798017e86f596f22
Paul Smith <code@uvwxy.de>
2016-02-09 20:44:47 +0100

    ui improvements

--- app/scripts/controllers/ng-reversi.js
+++ app/scripts/controllers/ng-reversi.js
@@ -9,22 +9,30 @@
  */
 angular.module('webReversiApp')
   .controller('NgReversiCtrl', function ($scope) {
-    $scope.game = new ABPrune.Game(ReversiLogic);
-    $scope.state = $scope.game.initialize();
-    $scope.isGameOver = false;
-    $scope.score1 = 2;
-    $scope.score2 = 2;
     $scope.hintsEnabled = true;
+    $scope.showOptions = false;
 
     $scope.hasHint = function (row, col) {
       return $scope.hintsEnabled && $scope.state.isMoveValid(row, col, $scope.state.player) ? 'hint' + $scope.state.player : '';
     };
 
-    $scope.game.initialize();
     $scope.$on('reversiClickField', function (event, row, col) {
-      ReversiLogicHelper.move($scope.state, row, col);
-      $scope.isGameOver = $scope.state.isGameOver();
-      $scope.score1 = ReversiLogicHelper.countPieces($scope.state, 1);
-      $scope.score2 = ReversiLogicHelper.countPieces($scope.state, 2);
+      if (!$scope.isGameOver) {
+        ReversiLogicHelper.move($scope.state, row, col);
+        $scope.isGameOver = $scope.state.isGameOver();
+        $scope.score1 = ReversiLogicHelper.countPieces($scope.state, 1);
+        $scope.score2 = ReversiLogicHelper.countPieces($scope.state, 2);
+      }
     });
+
+    $scope.newGame = function () {
+      $scope.game = new ABPrune.Game(ReversiLogic);
+      $scope.state = $scope.game.initialize();
+      $scope.isGameOver = false;
+      $scope.score1 = 2;
+      $scope.score2 = 2;
+    };
+
+
+    $scope.newGame();
   });
