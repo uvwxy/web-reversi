c9fb584ab7563fa3419bdea3cd90aac005bd4ab6
Paul Smith <code@uvwxy.de>
2016-02-10 22:56:47 +0100

    rename vars, fix handling of ai clicks

--- app/scripts/controllers/ng-reversi.js
+++ app/scripts/controllers/ng-reversi.js
@@ -11,8 +11,8 @@ angular.module('webReversiApp')
   .controller('NgReversiCtrl', function ($scope, $timeout) {
     $scope.hintsEnabled = true;
     $scope.showOptions = false;
-    $scope.ki1Enabled = false;
-    $scope.ki2Enabled = true;
+    $scope.ai1Enabled = false;
+    $scope.ai2Enabled = true;
     $scope.minDelay = 1000;
 
 
@@ -27,17 +27,17 @@ angular.module('webReversiApp')
         $scope.score1 = ReversiLogicHelper.countPieces($scope.state, 1);
         $scope.score2 = ReversiLogicHelper.countPieces($scope.state, 2);
 
-        if ($scope.ki1Enabled && $scope.state.player == 1) {
+        if ($scope.ai1Enabled && $scope.state.player == 1) {
           $timeout(function () {
             var move = new ABPrune.AlphaBeta(4, $scope.state).search().move;
-            ReversiLogicHelper.move($scope.state, move.row, move.col);
+            $scope.$emit('reversiClickField', move.row, move.col);
           }, $scope.minDelay);
-        } else if ($scope.ki2Enabled && $scope.state.player == 2) {
+        } else if ($scope.ai2Enabled && $scope.state.player == 2) {
           $timeout(function () {
             var copiedState = {data: ReversiLogicHelper.invert($scope.state.data)};
             $scope.state._copyFunctions(copiedState);
             var move = new ABPrune.AlphaBeta(8, copiedState).search().move;
-            ReversiLogicHelper.move($scope.state, move.row, move.col);
+            $scope.$emit('reversiClickField', move.row, move.col);
           }, $scope.minDelay);
         }
       }
