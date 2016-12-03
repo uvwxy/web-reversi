a1342b03ae64bdfb2e1e26fa37efdf0432f1f4f5
Paul Smith <code@uvwxy.de>
2016-12-03 16:19:27 +0100

    Fix invalid clicks/moves locking game

--- app/scripts/controllers/ng-reversi.js
+++ app/scripts/controllers/ng-reversi.js
@@ -22,24 +22,44 @@ angular.module('webReversiApp')
     };
 
     $scope.$on('reversiClickField', function (event, row, col) {
+      if ($scope.isProcFieldClick) {
+        return;
+      }
+      $scope.isProcFieldClick = true;
+
       if (!$scope.isGameOver) {
-        $scope.player = ReversiLogicHelper.move($scope.state, row, col, $scope.player);
+        var nextPlayer = ReversiLogicHelper.move($scope.state, row, col, $scope.player)
+        if (nextPlayer == -1){
+          // invalid move, don't do anything
+          $scope.isProcFieldClick = false;
+          return;
+        }
+        $scope.player = nextPlayer;
         $scope.isGameOver = $scope.state.isGameOver();
         $scope.score1 = ReversiLogicHelper.countPieces($scope.state, 1);
         $scope.score2 = ReversiLogicHelper.countPieces($scope.state, 2);
 
+        // check if other opponent needs to move
         if ($scope.ai1Enabled && $scope.player === 1) {
           $timeout(function () {
             var move = new ABPrune.AlphaBeta(4, $scope.state).search().move;
+
+            $scope.isProcFieldClick = false;
             $scope.$emit('reversiClickField', move.row, move.col);
           }, $scope.minDelay);
+
         } else if ($scope.ai2Enabled && $scope.player === 2) {
           $timeout(function () {
             var copiedState = {data: ReversiLogicHelper.invert($scope.state.data)};
             $scope.state._copyFunctions(copiedState);
             var result = new ABPrune.AlphaBeta(4, copiedState).search();
+
+            $scope.isProcFieldClick = false;
             $scope.$emit('reversiClickField', result.move.row, result.move.col);
           }, $scope.minDelay);
+        } else {
+          // no other ai moved, enable click
+          $scope.isProcFieldClick = false;
         }
       }
     });
